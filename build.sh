23e62f02ed6db9c9a1e17c2303b252a2db11268f
S-trace <S-trace@list.ru>
2016-04-19 14:46:26 +0300

    Revert "Raise make return code to shell called build.sh"
    This reverts commit 9a521f7e5e4e1632da75d3a07a7b9863bb6ac017.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mkdir -p ../../../out/target/product/d10f/obj/BOOTLOADER_EMMC_OBJ
 make DEBUG=2 PROJECT=msm8226 BOOTLOADER_OUT=../../../out/target/product/d10f/obj/BOOTLOADER_EMMC_OBJ EMMC_BOOT=1 VERSION="$(git describe --tags)" -j3
-exit $?
