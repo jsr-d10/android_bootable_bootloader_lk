33414d7c450e604f4f9279482e95ff7d1e87a9ed
S-trace <S-trace@list.ru>
2016-04-19 14:46:48 +0300

    build.sh: Show color indicator of build status

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+end=$(tput sgr0)
+
 mkdir -p ../../../out/target/product/d10f/obj/BOOTLOADER_EMMC_OBJ
 make DEBUG=2 PROJECT=msm8226 BOOTLOADER_OUT=../../../out/target/product/d10f/obj/BOOTLOADER_EMMC_OBJ EMMC_BOOT=1 VERSION="$(git describe --tags)" -j3
+if [ $? -gt 0 ]; then
+    echo ""
+    echo "${red}Build FAILED!${end}"
+else
+    echo ""
+    echo "${green}Successfully built${end}"
+fi
