4895b93047b88271bccf6afeb8b90b315340bb14
remittor <remittor@gmail.com>
2016-04-20 17:29:55 +0300

    aboot: Update eMMC speed test

--- app/aboot/advanced.c
+++ app/aboot/advanced.c
@@ -7,6 +7,7 @@
 #include <app/aboot/devinfo.h>
 #include <sdhci_msm.h>
 #include <dev/fbcon.h>
+#include <qtimer.h>
 #include "board.h"
 
 int get_storage_speed(uint32_t data_len, uint32_t buf_size)
@@ -41,32 +42,16 @@ int get_storage_speed(uint32_t data_len, uint32_t buf_size)
 	if (!buf)
 		return -2;
 
-	index = partition_get_index("userdata");
-	if (index == INVALID_PTN) {
-		dprintf(CRITICAL, "ERROR: userdata Partition table not found\n");
-		ret = -3;
-		goto exit;
-	}
-	ptn = partition_get_offset(index);
-	if (ptn == 0) {
-		dprintf(CRITICAL, "ERROR: userdata Partition invalid\n");
-		ret = -4;
-		goto exit;
-	}
-	ptn_size = partition_get_size(index);
-	if (ptn_size == 0) {
-		dprintf(CRITICAL, "ERROR: userdata Partition invalid\n");
-		ret = -5;
-		goto exit;
-	}
-
 	if (data_len % buf_size)
 		data_len = (data_len / buf_size) * buf_size;
 
-	ptn = ptn + ptn_size - data_len - (buf_size * 8);
+	ptn = dev->card.capacity + data_len + (block_size * 128);
 	ptn /= block_size;
 	blk_count = buf_size / block_size;
 	total_count = data_len / buf_size;
+
+	if (sdhci_wait_for_cmd(&dev->host, 10))
+		return -5;
 
 	t0 = qtimer_get_phy_timer_cnt();	
 	while (total_count && !ret) {
@@ -77,12 +62,18 @@ int get_storage_speed(uint32_t data_len, uint32_t buf_size)
 	t1 = qtimer_get_phy_timer_cnt();
 
 	if (ret) {
-		dprintf(CRITICAL, "Failed Reading block @ %llux (ret = %d) \n", (ptn * block_size), ret);
+		dprintf(CRITICAL, "Failed Reading block @ %llux (ret = %d) \n", ptn, ret);
 		ret = -6;
 		goto exit;
 	}
 
-	speed = ((uint64_t)data_len * qtimer_tick_rate()) / (t1 - t0);
+	if (t0 > t1) {
+		t0 = (QTMR_PHY_CNT_MAX_VALUE - t0) + t1;
+	} else {
+		t0 = t1 - t0;
+	}
+
+	speed = ((uint64_t)data_len * qtimer_tick_rate()) / t0;
 	if (speed > INT_MAX) {
 		ret = INT_MAX;
 	} else {
@@ -93,7 +84,7 @@ int get_storage_speed(uint32_t data_len, uint32_t buf_size)
 	{
 		uint32_t KiB = (uint32_t)speed / 1024;
 		uint32_t MiB = KiB / 1024;
-		uint64_t ms = (t1 - t0) * 1000;
+		uint64_t ms = t0 * 1000;
 		uint64_t us = (ms * 1000) / qtimer_tick_rate();
 		ms /= qtimer_tick_rate();
 		us -= ms * 1000;
@@ -113,6 +104,9 @@ void test_storage_read_speed(int storage)
 	struct mmc_device *mmc_dev = target_mmc_device();
 	int slot = SD_CARD;
 	int speed = 0;
+	int KiB = 0;
+	int MiB = 0;
+
 	dprintf(SPEW, "%s: entered\n", __func__);
 
 	if (mmc_dev)
@@ -129,14 +123,18 @@ void test_storage_read_speed(int storage)
 	fbcon_acprintf(2, ALIGN_CENTER, TEAL, "%s: Testing read speed", storage == EMMC_CARD ? "eMMC" : "SD");
 
 	speed = get_storage_speed(4 * MB, 4 * MB); // 4MiB data and 4MiB buffer is best settings for d10f
-	uint32_t KiB = (uint32_t)speed / 1024;
-	uint32_t MiB = KiB / 1024;
-	KiB -= MiB * 1024;
+	if (speed >= 0) {
+		KiB = speed / 1024;
+		MiB = KiB / 1024;
+		KiB -= MiB * 1024;
+	} else {
+		MiB = speed;
+	}
 
 	unsigned color = GREEN;
 	switch (storage) {
 		case EMMC_CARD:
-			if (MiB < 80)
+			if (MiB < 78)
 				color=YELLOW;
 			if (MiB < 72)
 				color=RED;
@@ -152,7 +150,7 @@ void test_storage_read_speed(int storage)
 	}
 	
 	fbcon_set_bg(BLACK, 0, 2, -1, 1);
-	fbcon_acprintf(2, ALIGN_CENTER, color, "%s: Read: %u.%03u MiB/s", storage == EMMC_CARD ? "eMMC" : "SD", MiB, KiB);
+	fbcon_acprintf(2, ALIGN_CENTER, color, "%s: Read: %d.%03d MiB/s", storage == EMMC_CARD ? "eMMC" : "SD", MiB, KiB);
 
 	dprintf(SPEW, "%s: done, speed=%d\n", __func__, speed);
 
