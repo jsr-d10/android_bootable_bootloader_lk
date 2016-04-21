821a07012ff5ef643db542cc456496fc273d10c9
S-trace <S-trace@list.ru>
2016-04-21 14:12:48 +0300

    build.sh: Add --boot option
    It allows build and boot IBL using fastboot boot without flashing it to storage

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ end=$(tput sgr0)
 build_dir="../../../out/target/product/d10f/obj/BOOTLOADER_EMMC_OBJ"
 
 print_usage() {
-    echo "Usage: $0 [-h|-?|--help] [-f|--flash] [-c|--clean]"
+    echo "Usage: $0 [-h|-?|--help] [-b|--boot] [-f|--flash] [-c|--clean]"
     echo "--help: show this text"
+    echo "--boot:  create bootable image and boot it to device using fastboot boot without flashing"
     echo "--flash: flash built binary to device using fastboot flash aboot and reboot device to fastboot"
     echo "--clean: run make clean before building"
 }
@@ -17,6 +18,7 @@ for arg in "$@"; do
   shift
   case "$arg" in
     "--help") set -- "$@" "-h" ;;
+    "--boot") set -- "$@" "-b" ;;
     "--clean") set -- "$@" "-c" ;;
     "--flash") set -- "$@" "-f" ;;
     *)        set -- "$@" "$arg"
@@ -24,9 +26,10 @@ for arg in "$@"; do
 done
 
 OPTIND=1
-while getopts "hfc" opt
+while getopts "hfcb" opt
 do
   case "$opt" in
+    "b") boot=true ;;
     "c") clean=true ;;
     "f") flash=true ;;
     "h") print_usage; exit 0 ;;
@@ -49,8 +52,15 @@ if [ $? -gt 0 ]; then
 else
     echo ""
     echo "${green}Successfully built${end}"
+    if [ "$boot" = "true" ]; then
+        rm -f "$build_dir/../../IBL.img" # Throw error on boot attempt if mkbootimg fails
+        mkbootimg --kernel "$build_dir/../../emmc_appsboot.raw" --dt "$build_dir/../../dt.img" --ramdisk /dev/null -o "$build_dir/../../IBL.img"
+        fastboot boot "$build_dir/../../IBL.img"
+        exit 0
+    fi
     if [ "$flash" = "true" ]; then
         fastboot flash aboot "$build_dir"/../../emmc_appsboot.mbn
         fastboot reboot bootloader
+        exit 0
     fi
 fi
