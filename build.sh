2ec04768f7550f567b000c04304b915ba1f42ee3
S-trace <S-trace@list.ru>
2018-01-12 23:17:10 +0300

    build.sh: Use nproc for make jobs count

--- build.sh
+++ build.sh
@@ -52,8 +52,7 @@ fi
 mkdir -p "$build_dir"
 
 touch dev/fbcon/fbcon.c # Force rebuilding it to make sure that version string is updated
-make DEBUG=2 PROJECT=msm8226 BOOTLOADER_OUT="$build_dir" EMMC_BOOT=1 VERSION="$VERSION" -j3
-if [ $? -gt 0 ]; then
+if ! make DEBUG=2 PROJECT=msm8226 BOOTLOADER_OUT="$build_dir" EMMC_BOOT=1 VERSION="$VERSION" -j"$(nproc)"; then
     echo ""
     echo "${red}Build FAILED!${end}"
 else
